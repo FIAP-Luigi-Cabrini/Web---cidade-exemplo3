d978114741be7cdf36246b06f3aa7a0f4d2e7b4a
Luigi Cabrini <luigi.mendes.cabrini@outlook.com>
2025-03-31 09:54:59 -0300

    final

--- src/js/script.js
+++ src/js/script.js
@@ -1,42 +1,48 @@
-// Declarações e váriaveis 
-var nome = "Fiap";
-console.log(nome);
+// // Declarações e váriaveis 
+// var nome = "Fiap";
+// console.log(nome);
 
-let nome1;
-console.log(nome1); //indefinida
+// let nome1;
+// console.log(nome1); //indefinida
 
-let nome2 = null;
-console.log(nome2);//variavel vazia
+// let nome2 = null;
+// console.log(nome2);//variavel vazia
 
-//Tipos de váriaveis
-let exemplo1 = 10;
-console.log(typeof exemplo1)
+// //Tipos de váriaveis
+// let exemplo1 = 10;
+// console.log(typeof exemplo1)
 
-let exemplo2 = "tecnologia";
-console.log(typeof exemplo2)
+// let exemplo2 = "tecnologia";
+// console.log(typeof exemplo2)
 
-let exemplo3 = true;
-console.log(typeof exemplo3)
+// let exemplo3 = true;
+// console.log(typeof exemplo3)
 
-let exemplo4 = {};
-console.log(typeof exemplo4)
+// let exemplo4 = {};
+// console.log(typeof exemplo4)
 
-let exemplo5 = [];
-console.log(typeof exemplo5)
+// let exemplo5 = [];
+// console.log(typeof exemplo5)
 
-//Conversões 
-let numFloat = 123.45;
-console.log(parseInt(numFloat))
+// //Conversões 
+// let numFloat = 123.45;
+// console.log(parseInt(numFloat))
 
-let numString = "123.456";
-console.log(parseFloat(numString))
+// let numString = "123.456";
+// console.log(parseFloat(numString))
 
-//metodos 
+// //metodos 
 
-let str1 = "Olá mundo"
-console.log(str1.length)// retorna o número de char da str
+// let str1 = "Olá mundo"
+// console.log(str1.length)// retorna o número de char da str
 
-console.log(str1.indexOf("á"))// retorna o índice 
+// console.log(str1.indexOf("á"))// retorna o índice 
 
-console.log(str1.slice(2,5))// corta a str
+// console.log(str1.slice(2,5))// corta a str
+
+
+//Operadores aritméticos 
+ const num1 = 10;
+ const num2 =20; 
+ console.log(num1 + num2)
 
